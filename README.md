5666ea57708861de6ebab5d1c980aebe4a2dd6b7
Pauline.B <paulinebanye@gmail.com>
2023-02-12 15:53:38 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# **Kinetic Aqueous Solubility**
+
+## **Model identifiers**
+- Slug: solubility
+- Ersilia ID: eos74bo
+- Tags: solubility
+#
+## **Model description**
+<p align="justify">
+Aqueous Solubility refers to the solubility of a pure substance or solute in a solvent. It measures the maximum concentration of a substance that can be completely dissolved in an aqueous medium at a certain temperature and pressure level. 
+</p>
+<p align="justify">
+A drug needs to dissolve and achieve the desired concentration to produce a desirable pharmacological response. Drugs that have poor aqueous solubility have a slower drug absorption rate, which can lead to inadequate and variable bioavailability, and render the drug ineffective. Poor solubility of drugs also leads to other issues, such as challenges with metabolism or permeability, interactions with other drugs or the need to extend drug release. 
+</p>
+<p align="justify">
+This data was provided by the National Center for Advancing Translational Sciences (NCATS). A probability of below 0.5 is considered high soluility. Probability of 0.5 or greater is considered low solubility. 
+</p>
+
+- Input: SMILES
+- Output: SMILES
+#
+## **Source code**
+
+Cite the source publication
+[Predictive models of aqueous solubility of organic compounds built on A large dataset of high integrity](https://pubmed.ncbi.nlm.nih.gov/31176566/)
+
+- Code: [NCATS-ADME](https://github.com/ncats/ncats-adme.git)
+- Checkpoints: include the link to the checkpoints used if model is a pretrained model
+#
+## **License**
+GNU General Public License v3.0.
+
+## **History**
+- The model was incorporated into Ersilia on the 22th of January, 2023.
+- Modifications to the original code.
+    1. Removal of Flask functionalities and dependencies.
+    2. Striping unused functions from the original code.
+
+- To run the model, follow these [steps](model/README.md).
+
+The [Ersilia Open Source Initiative](https://ersilia.io) is a Non Profit Organization ([1192266](https://register-of-charities.charitycommission.gov.uk/charity-search/-/charity-details/5170657/full-print)) with the mission is to equip labs, universities and clinics in LMIC with AI/ML tools for infectious disease research.
+
+[Help us](https://www.ersilia.io/donate) achieve our mission or [volunteer](https://www.ersilia.io/volunteer) with us!
---
+++ model/framework/code/main.py
@@ -0,0 +1,93 @@
+# imports
+import os
+import pandas as pd
+import csv
+import sys
+root = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(root, ".."))
+
+from os import path
+
+from predictors.solubility.solubility_predictor import SolubilityPredictor 
+from predictors.utilities.utilities import addMolsKekuleSmilesToFrame 
+
+
+# pass the input file
+input_file = sys.argv[1]
+# pass the output file
+output_file = sys.argv[2]
+
+
+def predict_df(
+    smiles_list, 
+    smi_column_name='smiles', 
+    models=['solubility']
+    ):
+    
+    df = pd.DataFrame({smi_column_name: smiles_list})    
+    response = {} 
+    working_df = df.copy() 
+    addMolsKekuleSmilesToFrame(working_df, smi_column_name) 
+    working_df = working_df[~working_df['mols'].isnull() & ~working_df['kekule_smiles'].isnull()] 
+
+    for model in models:
+        response[model] = {}
+        error_messages = []
+        
+        if model.lower() == 'solubility': 
+            predictor = SolubilityPredictor(
+                kekule_smiles = working_df['kekule_smiles'].values, 
+                smiles=working_df[smi_column_name].values
+                )
+        else:
+            break
+
+        pred_df = predictor.get_predictions()
+        pred_df = working_df.join(pred_df)
+        pred_df.drop(
+            ['mols', 'kekule_smiles'], 
+            axis=1, 
+            inplace = True
+            )
+
+        # columns not present in original df
+        diff_cols = pred_df.columns.difference(df.columns)
+        df_res = pred_df[diff_cols]
+
+        # making sure the response df is of the exact same length (rows) as original df
+        response_df = pd.merge(
+            df, 
+            df_res, 
+            left_index=True, 
+            right_index=True, 
+            how='inner'
+            )
+        return response_df
+      
+# read SMILES from .csv file, assuming one column with header
+with open(input_file, "r") as f:
+    reader = csv.reader(f)
+    next(reader)  # skip header
+    smiles_list = [r[0] for r in reader]
+
+# run model
+output_df = predict_df(smiles_list)
+
+OUTPUT_COLUMN_NAME = "Predicted Class (Probability)"
+
+outputs = []
+for x in list(output_df[OUTPUT_COLUMN_NAME]):
+    c = int(x.split(" ")[0])
+    p = float(x.split("(")[1].split(")")[0])
+    if c == 1:
+        outputs += [p]
+    else:
+        outputs += [1-p]
+print(outputs)
+
+# write output in a .csv file
+with open(output_file, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["value"])  # header
+    for o in outputs:
+        writer.writerow([o])
